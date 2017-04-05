c860f3c2c9571b2182cb5b596f7ffc058d07f3eb
Paige Plander <paigeplan@berkeley.edu>
2017-04-05 11:24:04 -0700

    Adding starter files

---
+++ Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Paige Plander on 4/5/17.
+//  Copyright © 2017 Paige Plander. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
