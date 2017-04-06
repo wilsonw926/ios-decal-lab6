bfb52d4c377373fdbab5eb8644667ab077b5c163
Paige Plander <paigeplan@berkeley.edu>
2017-04-06 16:54:21 -0700

    almost done

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -9,18 +9,53 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    // Constants used in the LoginViewController
+    struct Constants {
+        static let backgroundColor: UIColor = UIColor(hue: 0.5389, saturation: 1, brightness: 0.92, alpha: 1.0)
+        static let invalidEmailTitle = "Invalid username or password"
+        static let invalidEmailMessage = "Please try again"
+    }
+    
+    // Model class to handle checking if username/password combinations are valid
+    // usernames and passwords can be found in the Lab6Names.csv file
+    let loginModel = LoginModel(filename: "Lab6Names")
 
+    // TODO: instantiate the views needed for your project
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = Constants.backgroundColor
+        
+        // TODO: Add your views either as subviews of `view` or subviews of each other using `addSubview`
+        
+        // TODO: layout your views using frames or AutoLayout
     }
+    
+    // TODO: create an IBAction for your login button
 
+    
+    /// Displays an alert indicating whether or not the login was successful
+    /// You do not need to edit this function, but you will want to use it for the lab.
+    ///
+    /// - Parameters:
+    ///   - username: the user's berkeley.edu address
+    ///   - password: the user's first name (what a great password!)
+    func authenticateUser(username: String?, password: String?) {
+        if !loginModel.authenticate(username: username, password: password) {
+            let alert = UIAlertController(title: Constants.invalidEmailTitle, message: Constants.invalidEmailMessage, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        else {
+            // TODO paige: update this to display a view
+            print("logged in")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
